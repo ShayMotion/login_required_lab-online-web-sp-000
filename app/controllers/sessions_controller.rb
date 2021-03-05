21ed04e571b29d05d769aa3cbd4c71d353a554fe
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-03-05 04:08:51 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,31 @@
 class SessionsController < ApplicationController
-    before_action :require_login
-    
-  def index
-end
-  
+  # before_action :require_login
+
   def new
   end
 
-def create
-   @user = User.create(params(:user)
-   session[:user_id] = @user.id
-   redirect_to '/'
-end
+  def create
+    # binding.pry
+    name = params[:name]
+    if session[:name]
+      redirect_to "/secrets/show"
+    elsif name && name != ""
+      session[:name] = name
+      redirect_to "/secrets/show"
+    else
+      redirect_to "/sessions/new"
+    end
+  end
 
+  def destroy
+    if current_user
+      session[:name] = nil
+    end
+  end
 
-  def current_user
-end
+  private
 
+  def require_login
+    return head(:forbidden) unless session.include? :name
+  end
 end
